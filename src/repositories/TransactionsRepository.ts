4d924966e645a029ae9dd1636e6071fe49c88ccd
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-01-09 10:36:54 -0300

    atualizando repositório

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (prev, curr) => {
+        if (curr.type === 'outcome') {
+          return {
+            ...prev,
+            outcome: prev.outcome + curr.value,
+            total: prev.total - curr.value,
+          };
+        }
+
+        if (curr.type === 'income') {
+          return {
+            ...prev,
+            income: prev.income + curr.value,
+            total: prev.total + curr.value,
+          };
+        }
+
+        return prev;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(
+    type: 'income' | 'outcome',
+    title: string,
+    value: number,
+  ): Transaction {
+    const transaction = new Transaction({ type, title, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
