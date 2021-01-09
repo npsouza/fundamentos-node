a708901693c74acc86aa432cf4ae99a943646021
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-01-09 10:37:16 -0300

    atualizando service para criar transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  type: 'income' | 'outcome';
+  title: string;
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,15 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ type, title, value }: Request): Transaction {
+    const transaction = this.transactionsRepository.create(type, title, value);
+
+    const balance = this.transactionsRepository.getBalance();
+    if (transaction.type === 'outcome' && transaction.value > balance.total) {
+      throw Error('No balance for this transaction');
+    }
+
+    return transaction;
   }
 }
 
